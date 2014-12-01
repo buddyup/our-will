59a93d1d30aef021301d13acd3c424f468a89f3d
Steven Skoczen <steven@quantumimagery.com>
2014-12-01 12:38:15 +0700

    Adds a biz plugin, with sales cycle.

---
+++ plugins/biz/sales_cycle.py
@@ -0,0 +1,15 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+
+
+class SalesCyclePlugin(WillPlugin):
+
+    @route("/api/sales-update", method="POST")
+    def deploy_notification(self):
+        assert self.request.json and "name" in self.request.json
+        payload = self.request.json
+        message = rendered_template("sales_update.html", context=payload)
+        color = "green"
+        self.say(message, html=True, color=color)
+
+        return "OK"
