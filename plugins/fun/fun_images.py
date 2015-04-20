4f4405e6ed1df5724ecdd3e229d065f14ff31990
John Skelton <john@afrojet.com>
2015-04-20 09:26:25 -0700

    make it rain, Will!

--- plugins/fun/fun_images.py
+++ plugins/fun/fun_images.py
@@ -8,3 +8,8 @@ class FunImagesPlugin(WillPlugin):
     def hear_highfive(self, message):
         """high 5: Will's got spirit."""
         self.say("https://buddyup-will.s3.amazonaws.com/highfive.jpg", message=message)
+
+    @hear("(money|dollars?)")
+    def hear_money(self, message):
+        """money: make it rain."""
+        self.say("https://s3.amazonaws.com/uploads.hipchat.com/183747/1970469/ymR4Tivk6jwgRH7/Kenny-Powers-Dolla.gif", message=message)
