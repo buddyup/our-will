e9089740d05f93f5bccd2d229e8d365fc7cb94a0
Steven Skoczen <steven@quantumimagery.com>
2015-05-11 11:07:12 -0500

    Teaches will standup is at 8:30

--- plugins/culture/standup.py
+++ plugins/culture/standup.py
@@ -2,9 +2,10 @@ from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 from will import settings
 
+
 class StandupPlugin(WillPlugin):
 
-    @periodic(hour='8', minute='45', day_of_week="mon-fri")
+    @periodic(hour='8', minute='30', day_of_week="mon-fri")
     @require_settings("ZOOM_URL")
     def standup(self):
         self.say("@here Standup! %s" % settings.ZOOM_URL)
