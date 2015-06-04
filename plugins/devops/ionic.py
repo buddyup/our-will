0d105c82e96fcdb808292db24cc41bb6c845f48a
Steven Skoczen <steven@quantumimagery.com>
2015-06-04 15:18:57 -0500

    Gives will the new ionic code

--- plugins/devops/ionic.py
+++ plugins/devops/ionic.py
@@ -4,10 +4,11 @@ from will.decorators import respond_to
 
 class IonicPlugin(WillPlugin):
 
-    @respond_to("^qr")
+    @respond_to("^(qr|ionic)")
     def send_qr_image(self, message):
-        self.say(
-            "https://s3.amazonaws.com/uploads.hipchat.com/183747/1321383/75jBtlWrcnnmHea/upload.png",
-            message=message
-        )
-        self.say("CAE63470", message=message)
+        # self.say(
+            # "https://s3.amazonaws.com/uploads.hipchat.com/183747/1321383/75jBtlWrcnnmHea/upload.png",
+            # message=message
+        # )
+        self.say("App id is:", message=message)
+        self.say("/code 0de1e300", message=message)
