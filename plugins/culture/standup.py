1a50e9296ff4bf25f4a35c774fc5e3e44ec54708
Steven Skoczen <steven@quantumimagery.com>
2015-09-30 11:44:26 -0500

    Teaches will emoji!

--- plugins/culture/standup.py
+++ plugins/culture/standup.py
@@ -8,5 +8,5 @@ class StandupPlugin(WillPlugin):
     @periodic(hour='9', minute='30', day_of_week="mon-fri")
     @require_settings("ZOOM_URL")
     def standup(self):
-        self.say("@here Standup! %s" % settings.ZOOM_URL)
+        self.say("@here (standup) Standup! %s" % settings.ZOOM_URL)
         self.say("Launch plan: https://groundcontrol.buddyup.org/#/app/launch_list")
