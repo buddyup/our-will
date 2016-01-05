4e6d0757d228bcd1858434266469eaeb56ef7a34
Steven Skoczen <steven@quantumimagery.com>
2016-01-05 12:25:23 -0500

    Teaches will about values

--- plugins/culture/monthly_goals.py
+++ plugins/culture/monthly_goals.py
@@ -1,6 +1,21 @@
+import random
 from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 from will import settings
+
+VALUES = [
+    "Radical Authenticity. Be who we are, fully, with honesty, transparency, and passion.",
+    "Do the Right Thing. Ethically and personally, do the right thing for each other, our users, and the world.",
+    "Build Real Relationships. Intentionally focus on building and strengthening genuine relationships between students, clients, and each other.",
+    "Listen. Use Active Listening. Seek feedback. Listen fully before talking.",
+    "Communicate Clearly and Often. Spend the effort to communicate well, in writing and speech.  Communicate more often than you think is necessary. Never say sorry.",
+    "Use Science. Hypothesize, Experiment, Measure, Learn.",
+    "Invest in Tools. Turn knowledge and processes into people-independent tools.",
+    "Work Where, When and How You're at Your Best. Find the spot where you’re at your best, and make the most of it.  Bring your best self and best hours to BuddyUp.",
+    "Seek Discomfort. Continually and intentionally put yourself in situations where you'll grow.",
+    "Get Shit Done. Be a proactive, no-ego doer.",
+]
+
 
 class MonthlyGoalsPlugin(WillPlugin):
 
@@ -20,10 +35,13 @@ class MonthlyGoalsPlugin(WillPlugin):
         """what are the monthly goals?: Posts the monthly goals."""
         self.say_goals(message=message)
 
-
     def say_goals(self, message=None):
         goals = self.load("monthly_goals", False)
         if goals:
             self.say("@all our monthly goals:\n %s" % goals, message=message, html=True)
         else:
             self.say("No monthly goals set.", message=message)
+
+    @periodic(hour='10', minute='0', day_of_week="mon-fri")
+    def say_value(self):
+        self.say("@here here's a value to focus on today: %s" % random.choice(VALUES))
