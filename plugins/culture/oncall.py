25126f25a2c0e284092ac85bcd540824eb7b6c65
Steven Skoczen <steven@inkandfeet.com>
2016-06-23 12:53:18 -0300

    pulls on call for now.

--- plugins/culture/oncall.py
+++ plugins/culture/oncall.py
@@ -22,33 +22,33 @@ class OnCallPlugin(WillPlugin):
         self.say("@%s you're on call for support today!" % user, message=message)
         self.set_topic("Support: @%s | FAQ: http://goo.gl/pX1mhU" % user, message=message)
 
-    @periodic(hour='8', minute='0', day_of_week="mon")
-    def support_mon(self):
-        self.support_handler("mon")
+    # @periodic(hour='8', minute='0', day_of_week="mon")
+    # def support_mon(self):
+    #     self.support_handler("mon")
 
-    @periodic(hour='8', minute='0', day_of_week="tue")
-    def support_tue(self):
-        self.support_handler("tue")
+    # @periodic(hour='8', minute='0', day_of_week="tue")
+    # def support_tue(self):
+    #     self.support_handler("tue")
 
-    @periodic(hour='8', minute='0', day_of_week="wed")
-    def support_wed(self):
-        self.support_handler("wed")
+    # @periodic(hour='8', minute='0', day_of_week="wed")
+    # def support_wed(self):
+    #     self.support_handler("wed")
 
-    @periodic(hour='8', minute='0', day_of_week="thu")
-    def support_thu(self):
-        self.support_handler("thu")
+    # @periodic(hour='8', minute='0', day_of_week="thu")
+    # def support_thu(self):
+    #     self.support_handler("thu")
 
-    @periodic(hour='8', minute='0', day_of_week="fri")
-    def support_fri(self):
-        self.support_handler("fri")
+    # @periodic(hour='8', minute='0', day_of_week="fri")
+    # def support_fri(self):
+    #     self.support_handler("fri")
 
-    @periodic(hour='8', minute='0', day_of_week="sat")
-    def support_sat(self):
-        self.support_handler("sat")
+    # @periodic(hour='8', minute='0', day_of_week="sat")
+    # def support_sat(self):
+    #     self.support_handler("sat")
 
-    @periodic(hour='8', minute='0', day_of_week="sun")
-    def support_sun(self):
-        self.support_handler("sun")
+    # @periodic(hour='8', minute='0', day_of_week="sun")
+    # def support_sun(self):
+    #     self.support_handler("sun")
 
     @respond_to("I'm on call")
     def support_today(self, message):
